d5af2eff08c03f6d660a57602267bf7a65cdd500
ndarcy201 <njd57@pitt.edu>
2023-09-15 14:29:27 -0400

    Added CityImpl

--- src/main/java/hotciv/standard/CityImpl.java
+++ src/main/java/hotciv/standard/CityImpl.java
@@ -1,2 +1,11 @@
-package hotciv.standard;public class CityImpl {
+package hotciv.standard;
+
+import hotciv.framework.*;
+
+public class CityImpl implements City{
+    public Player getOwner() { return null;}
+    public int getSize(){ return 1;}
+    public int getTreasury(){ return 0;}
+    public String getProduction(){ return null;}
+    public String getWorkforceFocus(){ return null;}
 }
